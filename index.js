e0d2dfb405ec6d94c2cbb8d0147e06e9e85f7fe7
IBra <Ibrahima.Bah@hardis.fr>
2019-01-09 16:59:00 +0100

    Add userNewParser option

--- index.js
+++ index.js
@@ -2,14 +2,12 @@ const express = require('express');
 const routes = require('./routes/api');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
 
 // brings express app to life
 const app = express();
 const port = process.env.port || 4000;
 //connect to mongodb
-mongoose.connect('mongodb://localhost/books');
+mongoose.connect('mongodb://localhost:27017/books', { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 
 app.use(bodyParser.json());
